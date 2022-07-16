13ed24f1ca08534cd12480416445d104da345fa4
Peter <sceptix@gmail.com>
2022-07-16 12:30:17 -0400

    up up and away

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ const SCREEN_HEIGHT: f32 = 1000.0;
 const SCREEN_HEIGHT_OFFSET: f32 = SCREEN_HEIGHT / 2.0;
 const SCREEN_WIDTH_OFFSET: f32 = SCREEN_WIDTH / 2.0;
 
+const BLOCK_SPRITE_SIZE: (f32,f32) = (70.0,70.0);
+
+const ACTOR_SPRITE_SIZE: (f32,f32) = (70.0,70.0);
 
 
 fn main() {
@@ -28,10 +31,12 @@ pub struct GameSetup;
 impl Plugin for GameSetup {
     fn build(&self, app: &mut App) {
         app
-        .add_system(game_setup_system)
-        .add_system(actor_setup_system)
-        .add_system(block_setup_system)
-        .add_system(asset_setup_system);
+        .add_startup_system(actor_setup_system)
+        .add_startup_system(block_setup_system)
+        .add_startup_system(game_setup_system)
+        .add_system(asset_setup_system)
+        .add_system(actor_move_system);
+        // .add_system(actor_move_system);
     }
 }
 
@@ -40,12 +45,28 @@ fn game_setup_system(mut commands: Commands) {
 
 }
 
-fn asset_setup_system(mut commands: Commands, asset_server: Res<AssetServer> ) {
+#[derive(Component)]
+pub struct GameAssets{
+    pub actor_animation_sprite: Handle<TextureAtlas>,
+}
+
+fn asset_setup_system(mut commands: Commands, asset_server: Res<AssetServer>, mut atlas_server: ResMut<Assets<TextureAtlas>> ) {
+    const ACTOR_ANIMATE_SPRITE: &str = "laserRightFire-sprites.png";
+    const ACTOR_ANIMATE_SPRITE_ROWS: usize = 4;
+    const ACTOR_ANIMATE_SPRITE_COLS: usize = 4;
+
+    let texture_handle = asset_server.load(ACTOR_ANIMATE_SPRITE);
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(ACTOR_SPRITE_SIZE.0,ACTOR_SPRITE_SIZE.1), ACTOR_ANIMATE_SPRITE_COLS, ACTOR_ANIMATE_SPRITE_ROWS);
+    let texture_atlas_handle = atlas_server.add(texture_atlas);
+
+    commands.insert_resource(GameAssets { actor_animation_sprite: texture_atlas_handle});
+
 }
 
 fn block_setup_system(mut commands: Commands, asset_server: Res<AssetServer>){
     const BLOCK_SPRITE: &str = "metalCenter.png";
-    const BLOCK_SPRITE_SIZE: (f32,f32) = (70.0,70.0);
     const BLOCK_SCALE: f32 = 1.0;
     const BLOCK_SPRITE_OFFSET: f32 = BLOCK_SPRITE_SIZE.0 / 2.0;
 
@@ -56,27 +77,38 @@ fn block_setup_system(mut commands: Commands, asset_server: Res<AssetServer>){
         .spawn_bundle(SpriteBundle {
             texture: asset_server.load(BLOCK_SPRITE),
             transform: Transform {
-            scale: Vec3::new(BLOCK_SCALE, BLOCK_SCALE, 1.),
-            translation: Vec3::new(x, y, 2.0),
-            ..Default::default()
+                scale: Vec3::new(BLOCK_SCALE, BLOCK_SCALE, 1.),
+                translation: Vec3::new(x, y, 2.0),
+                ..Default::default()
             },
             ..Default::default()
             });
 }
 
 #[derive(Component)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Component)]
 pub struct Actor;
 
-fn _actor_move_system(mut commands: Commands, query: Query<(&Transform), With<Actor>>) {
+fn actor_move_system(mut commands: Commands, mut query: Query<(&Velocity, &mut Transform), With<Actor>>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.x += velocity.x;
+        translation.y += velocity.y * 10.0;
+    }
 }
 
 fn actor_setup_system(mut commands: Commands, asset_server: Res<AssetServer>){
     const ACTOR_SPRITE: &str = "laserUp.png";
-    const _ACTOR_SPRITE_SIZE: (f32,f32) = (70.0,70.0);
     const ACTOR_SCALE: f32 = 1.0;
-    const ACTOR_SPRITE_OFFSET: f32 = _ACTOR_SPRITE_SIZE.0 / 2.0;
+    const ACTOR_SPRITE_OFFSET: f32 = ACTOR_SPRITE_SIZE.0 / 2.0;
 
     let (x,y) = (-SCREEN_WIDTH_OFFSET + ACTOR_SPRITE_OFFSET,-SCREEN_HEIGHT_OFFSET + ACTOR_SPRITE_OFFSET);
+    // let (x,y) = (0.0,1.0);
 
     commands
         .spawn_bundle(SpriteBundle {
@@ -84,10 +116,11 @@ fn actor_setup_system(mut commands: Commands, asset_server: Res<AssetServer>){
             transform: Transform {
                 scale: Vec3::new(ACTOR_SCALE, ACTOR_SCALE, 1.),
                 translation: Vec3::new(x, y, 2.0),
-                rotation: Quat::from_rotation_z(4.71_f32),
+                rotation: Quat::from_rotation_z(4.71_f32),  // rads is 270 degrees counter-clockwise
                 ..Default::default()
             },
             ..Default::default()
         })
-        .insert(Actor);
+        .insert(Actor)
+        .insert(Velocity { x:0.0, y:1.0 });
 }
